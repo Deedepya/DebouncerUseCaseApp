baf0b8370285dd0b6c73042ef5942e33bd26bf4f
Deedepya <dedeepyaitech@gmail.com>
2025-08-04 22:21:33 -0500

    Created iOS project

---
+++ iOSDebouncerUseCase/iOSDebouncerUseCase/iOSDebouncerUseCaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSDebouncerUseCaseApp.swift
+//  iOSDebouncerUseCase
+//
+//  Created by dedeepya reddy salla on 04/08/25.
+//
+
+import SwiftUI
+
+@main
+struct iOSDebouncerUseCaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
